084f2863709995bcc2e8dcf92c88af4358406d8b
Jorge-Bill <jorge.mendesx@gmail.com>
2020-09-02 17:24:20 -0300

    blog responsive

--- src/components/Search/styled.js
+++ src/components/Search/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import media from "styled-media-query"
 
 export const SearchWrapper = styled.section`
   background: var(--background);
@@ -17,6 +18,10 @@ export const SearchWrapper = styled.section`
   .ais-SearchBox,
   .ais-Stats {
     padding: 0.5rem 3rem;
+
+    ${media.lessThan("large")`
+      padding: 0.5rem 1rem;
+    `}
   }
 
   body#grid & {
@@ -37,6 +42,10 @@ export const SearchWrapper = styled.section`
 
   .ais-SearchBox {
     padding-top: 6rem;
+
+    ${media.lessThan("large")`
+      padding-top: 1rem;
+    `}
   }
   .ais-Stats {
     color: white;
