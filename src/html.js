2178815d44a0ddff4889581436066850f281483f
Jorge Mendes <jorge.mendesx@gmail.com>
2020-09-17 23:01:51 -0300

    add metatag google verification

--- src/html.js
+++ src/html.js
@@ -11,6 +11,10 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <meta 
+          name="google-site-verification" 
+          content="khzGXPM--SlAFG6V0_NIO4ITwpai3NBnCEtvgEMOlO8" 
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes} className="light">
