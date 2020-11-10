549d485bca0bda86c2c7975b640c8b205497edb1
Jorge-Bill <jorge.mendesx@gmail.com>
2020-11-10 19:07:55 -0300

    add auto reload

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,15 @@
 import "lazysizes"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SwUpdater from './src/components/SwUpdater'
 require("prismjs/themes/prism-tomorrow.css")
-export function onServiceWorkerUpdateReady(){
-  window.location.reload(true)
-}
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
 
-// You can delete this file if you're not using it
+function onServiceWorkerUpdateReady() {
+  const root = document.createElement('div')
+  document.body.appendChild(root)
+  console.log('SW Asks for update!')
+
+  ReactDOM.render(<SwUpdater />, root)
+}
+
+export { onServiceWorkerUpdateReady }
