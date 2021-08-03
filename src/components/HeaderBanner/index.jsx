7ec2fea5f9d213cffae65b89b9d5fe12a2a607f8
Jorge Mendes <jorge.mendes@fdte.io>
2021-08-03 16:52:22 -0300

    loader and lazy load banner

--- src/components/HeaderBanner/index.jsx
+++ src/components/HeaderBanner/index.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 
 import PropTypes from 'prop-types'
+import LazyLoad from "react-lazyload"
 
 import * as S from './styles'
 
-const HeaderBanner = ({ title, tags, ...props }) => (
-  <S.BannerWrapper {...props} tags={tags}>
-    <S.BannerTitle>{title}</S.BannerTitle>
-  </S.BannerWrapper>
-)
+const HeaderBanner = ({ title, tags, ...props }) => {
+  const refLoader = React.useRef();
+  const removeLoader = () => {
+    refLoader.current.remove();
+  };
+
+  return (
+    <S.BannerWrapper {...props}>
+      <LazyLoad>
+        <S.BannerTitle>{title}</S.BannerTitle>
+        <S.Loader ref={refLoader} />
+        <S.ImageBanner
+          src={`https://source.unsplash.com/1600x900/?desktop,${tags}`}
+          alt={tags}
+          onLoad={removeLoader}
+          onError={removeLoader} />
+      </LazyLoad>
+    </S.BannerWrapper>
+  )
+}
 
 HeaderBanner.propTypes = {
   title: PropTypes.string,
@@ -20,4 +36,4 @@ HeaderBanner.defaultProps = {
   tags: 'nature,star,galaxy,space'
 }
 
-export default HeaderBanner
+export default React.memo(HeaderBanner)
--- src/components/HeaderBanner/styles.js
+++ src/components/HeaderBanner/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import media from "styled-media-query"
 
 export const BannerWrapper = styled.section`
@@ -12,25 +12,66 @@ export const BannerWrapper = styled.section`
   transition: .15s;
   opacity: 0.8;
 
-  ${media.greaterThan("medium")`
-    background: url(https://source.unsplash.com/1600x900/?desktop,${props => props.tags});
-  `}
-
-  ${media.greaterThan("huge")`
-    background: url(https://source.unsplash.com/3823x2510/?desktop,${props => props.tags});
-  `}
-
-  background-attachment: fixed;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-
   ${media.lessThan("large")`
     display: none;
   `}
 `
 
+export const ImageBanner = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
+`
+
+const loadingAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(180deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Loader = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+
+  transform: translateZ(0);
+  animation: ${loadingAnimation} 1.1s infinite linear;
+
+
+  margin: 60px auto;
+  font-size: 10px;
+  position: relative;
+  text-indent: -9999em;
+  border-top: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-right: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-bottom: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-left: 1.1em solid #ffffff;
+
+  &:after {
+    border-radius: 50%;
+    width: 50em;
+    height: 50em;
+  }
+`;
+
 export const BannerTitle = styled.h1`
+  position: absolute;
+  z-index: 999;
+  left:0;
+  right:0;
+
+  text-align: center;
   color:  var(--postColor);
   text-transform: uppercase;
   font-size: 6rem;
