b2094470b7c4146ce4060f8020af84180656d503
Jorge Mendes <jorge.mendes@fdte.io>
2021-08-09 12:54:33 -0300

    component loader

--- src/components/HeaderBanner/styles.js
+++ src/components/HeaderBanner/styles.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 import media from "styled-media-query"
 
 export const BannerWrapper = styled.section`
@@ -25,37 +25,6 @@ export const ImageBanner = styled.img`
   object-fit: cover;
   opacity: 0.8;
 `
-
-const loadingAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    transition: opacity 0.5s;
-  }
-  100% {
-    transform: rotate(360deg);
-    transition: opacity 0.8s;
-  }
-`;
-
-export const Loader = styled.div`
-  margin: auto;
-  position: absolute;
-  top: 35vh;
-  left: 0;
-  right: 0;
-  z-index: 1;
-
-  width: 20rem;
-  height: 20rem;
-  border-radius: 50%;
-
-  animation: ${loadingAnimation} .5s linear infinite;
-  border: 1em solid var(--postColor);
-  border-image-source: linear-gradient(to left,(--postColor), var(--borders));
-  border-top: 1em solid var(--highlight);
-  box-shadow: 0 4px 8px 0 var(--highlight);
-  opacity 0.7;
-`;
 
 export const BannerTitle = styled.h1`
   position: absolute;
