682480418b3ee41fc0ee2a50ba79e65828e7dab7
Jorge Silva <jorge.silva>
2021-10-15 17:54:59 -0300

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,27 +16,27 @@ module.exports = {
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     {
-      resolve: 'gatsby-plugin-prettier-eslint',
+      resolve: "gatsby-plugin-prettier-eslint",
       options: {
         prettier: {
           patterns: [
             // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
-            '**/*.{css,scss,less}',
-            '**/*.{json,json5}',
-            '**/*.{graphql}',
-            '**/*.{md,mdx}',
-            '**/*.{html}',
-            '**/*.{yaml,yml}'
-          ]
+            "**/*.{css,scss,less}",
+            "**/*.{json,json5}",
+            "**/*.{graphql}",
+            "**/*.{md,mdx}",
+            "**/*.{html}",
+            "**/*.{yaml,yml}",
+          ],
         },
         eslint: {
-          patterns: '**/*.{js,jsx,ts,tsx}',
+          patterns: "**/*.{js,jsx,ts,tsx}",
           customOptions: {
             fix: true,
-            cache: true
-          }
-        }
-      }
+            cache: true,
+          },
+        },
+      },
     },
     // needs to be fist to work with gatsby-remark-images
     {
